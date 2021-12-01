292247f4846b7a8b9ea836aeafcd5eaa94955dfe
Caleb Owens <Cocacola5010mc@Gmail.com>
2021-12-01 21:58:54 +0000

    Day one

---
+++ src/one.rb
@@ -0,0 +1,28 @@
+file = File.open('one.txt', chomp: true)
+
+data = file.readlines.map do | entry |
+    entry.to_i
+end
+
+increases = 0
+
+data.drop(1).each_with_index do | element, index |
+    if element > data[index]
+        increases += 1
+    end
+end
+
+puts "Increases: ", increases
+
+batchedIncreases = 0
+
+data.drop(3).each_index do | index |
+    first = data[index] + data[index + 1] + data[index + 2]
+    second = data[index + 1] + data[index + 2] + data[index + 3]
+
+    if second > first
+        batchedIncreases += 1
+    end
+end
+
+puts "Batched Increases: ", batchedIncreases
