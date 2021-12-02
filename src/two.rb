bf1429e3607bf743e2b22029f4776235ee7f7ac8
Caleb Owens <Cocacola5010mc@Gmail.com>
2021-12-02 23:04:19 +0000

    Day two

---
+++ src/two.rb
@@ -0,0 +1,56 @@
+file = File.open('two.txt', chomp: true)
+
+depth = 0
+distance = 0
+
+class Statement < String
+    def key()
+        split.first()
+    end
+
+    def value()
+        split[1].to_i()
+    end
+end
+
+lines = file.readlines()
+
+lines.each do | rawStatement |
+    statement = Statement.new(rawStatement)
+
+    case statement.key()
+    when "forward"
+        distance += statement.value()
+    when "down"
+        depth += statement.value()
+    when "up"
+        depth -= statement.value()
+    end
+end
+
+puts "Depth: #{depth}, Distance: #{distance}, Final: #{depth * distance}"
+
+depth = 0
+distance = 0
+
+pointer = 0
+
+lines.each do | rawStatement |
+    statement = Statement.new(rawStatement)
+
+    case statement.key()
+    when "forward"
+        increment = statement.value()
+
+        distance += increment
+        if pointer != 0
+            depth += increment * pointer
+        end
+    when "down"
+        pointer += statement.value()
+    when "up"
+        pointer -= statement.value()
+    end
+end
+
+puts "Depth: #{depth}, Distance: #{distance}, Pointer: #{pointer} Final: #{depth * distance}"
